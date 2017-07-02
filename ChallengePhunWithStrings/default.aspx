4b354435852dde97c3af8192343fd407b1f39b41
NashNWood <nashua.wood@gmail.com>
2017-07-01 22:04:40 -0700

    Add project files.

---
+++ ChallengePhunWithStrings/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengePhunWithStrings._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    </div>
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengePhunWithStrings/default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //1. Reverse your name
+            /*string name = "Nash Wood";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                resultLabel.Text += name[i];
+            }
+            */
+
+            //2. Reverse this sequence
+            string names = "Luke,Leia,Han,Chewbacca";
+            string result = "";
+            string[] rebelScum = names.Split(',');
+            /*for (int j = rebelScum.Length - 1; j >= 0; j--)
+            {
+                result += rebelScum[j] + ",";
+            }
+            result = result.Remove(result.Length - 1, 1);
+            resultLabel.Text = result;
+            */
+
+            // 3.Use the sequence to create ascii art
+
+
+
+        }
+
+
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+
+        
+    }
