ffc64f039b5ee05b834bee5ba9dadb620ea53875
NashNWood <nashua.wood@gmail.com>
2017-07-04 10:39:17 -0700

    Finished assignment 4.

--- ChallengePhunWithStrings/default.aspx.cs
+++ ChallengePhunWithStrings/default.aspx.cs
@@ -20,9 +20,9 @@ namespace ChallengePhunWithStrings
             */
 
             //2. Reverse this sequence
-            string names = "Luke,Leia,Han,Chewbacca";
-            string result = "";
-            string[] rebelScum = names.Split(',');
+            //string names = "Luke,Leia,Han,Chewbacca";
+            //string result = "";
+            //string[] rebelScum = names.Split(',');
             /*for (int j = rebelScum.Length - 1; j >= 0; j--)
             {
                 result += rebelScum[j] + ",";
@@ -32,10 +32,41 @@ namespace ChallengePhunWithStrings
             */
 
             // 3.Use the sequence to create ascii art
+            //*****Luke*****
+            //*****Leia*****
+            //*****Han******
+            //**Chewbacca***
 
+            /*
+            string[] rebelScum = names.Split(',');
+            string result = "";
+            for (int i = 0; i < rebelScum.Length; i++)
+            {
+                int padLeft = (14 - rebelScum[i].Length) / 2;
+                string temp = rebelScum[i].PadLeft(rebelScum[i].Length + padLeft, '*');
+                result += temp.PadRight(14, '*');
+                result += "<br />";  
+            }
+            resultLabel.Text = result;
+            */
 
+            // 4. Solve this puzzle:
 
-        }
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+           
+            // Once you fix it with string helper methods, it should read:
+            // "Now is the time for all good men to come to the aid of their country."
+
+            string removeMe = "remove-me";
+            int index = puzzle.IndexOf(removeMe);
+            puzzle = puzzle.Remove(index, removeMe.Length);
+            puzzle = puzzle.ToLower();
+            puzzle = puzzle.Replace("z", "t");
+            puzzle = puzzle.Remove(0, 1);
+            puzzle = puzzle.Insert(0, "N");
+            
+            resultLabel.Text = puzzle;
+    }
 
 
         
